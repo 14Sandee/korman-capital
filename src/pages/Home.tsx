19a951e5e1a3b94f274f087583587000004a76e4
14Sandee <sandee.vit14@gmail.com>
2024-01-12 10:00:42 +0530

    footer changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,21 +15,21 @@ export const Home = () => {
                 <Box maxW={{ base: 'xl', md: '7xl' }} mx="auto" px={{ base: '6', md: '8' }}>
                     <Stack spacing={{ base: 10, md: 20 }}>
                         <Stack spacing={0} alignItems={'center'}>
-                            <Heading as="h2" size="2xl" mt="8" fontWeight={400} color={'primary.500'}>Invest in the
+                            <Heading as="h2" size={{ base: 'xl', md: '2xl' }} mt="8" fontWeight={400} color={'primary.500'}>Invest in the
                                 <Text as='span' color={'secondary.500'} fontFamily={'RecifeDisplay-Italic'} fontWeight={'semibold'}> India opportunity</Text>
                             </Heading>
-                            <Image mt={-10} w={'full'} src={HeroImage} alt="hero image" />
+                            <Image mt={{ base: 0, md: -10 }} w={'full'} src={HeroImage} alt="hero image" />
                         </Stack>
                         <Stack px={{ base: 0, md: 10 }} spacing={4}>
-                            <Heading mb={4} fontSize={'4xl'} color={'secondary.500'} fontFamily={'RecifeDisplay-Italic'} fontWeight={'semibold'}>Korman Capital is a Sebi registered investment advisory firm.</Heading>
+                            <Heading mb={4} fontSize={{ base: '2xl', md: '4xl' }} color={'secondary.500'} fontFamily={'RecifeDisplay-Italic'} fontWeight={'semibold'}>Korman Capital is a Sebi registered investment advisory firm.</Heading>
                             <Text fontSize={{ md: 18, base: 16 }} fontWeight={500} lineHeight={'md'}>Korman Capital, founded by an experienced investor with a background in Flipkart, ICICI Bank, and Barclays, specializes in Indian capital markets. With over a decade of investment expertise, our founder transitioned from a corporate career to pursue a passion for equities. For the past 6 years, Korman Capital has thrived under his guidance, delivering consistent above-average returns, navigating market fluctuations, and harnessing the power of compounding for sustainable growth.</Text>
                         </Stack>
                         <Stack px={{ base: 0, md: 10 }} spacing={4}>
-                            <Heading mb={4} fontSize={'4xl'} color={'secondary.500'} fontFamily={'RecifeDisplay-Italic'} fontWeight={'semibold'}>Clientele</Heading>
+                            <Heading mb={4} fontSize={{ base: '2xl', md: '4xl' }} color={'secondary.500'} fontFamily={'RecifeDisplay-Italic'} fontWeight={'semibold'}>Clientele</Heading>
                             <SimpleGrid columns={[1, 4]} spacing={4}>
                                 {data.map((item: ClientsProps) => <Stack spacing={0} rounded={20} overflow={'hidden'}>
                                     <Stack spacing={4} px={{ base: 4, md: 8 }} py={{ base: 4, md: 8 }} bg={'primary.500'} >
-                                        <Heading fontSize={'3xl'} color={'white'} textAlign={'center'} fontFamily={'RecifeDisplay'} fontWeight={'regular'}>{item.name}</Heading>
+                                        <Heading fontSize={{ md: '3xl', base: 'xl' }} color={'white'} textAlign={'center'} fontFamily={'RecifeDisplay'} fontWeight={'regular'}>{item.name}</Heading>
                                     </Stack>
                                     <Stack spacing={4} px={{ base: 4, md: 8 }} py={{ base: 4, md: 8 }} bg={'secondary.500'} >
                                         {item.clients.map((client) => <Text fontSize={{ md: 18, base: 16 }} fontWeight={500} color={'white'}>{client}</Text>)}
